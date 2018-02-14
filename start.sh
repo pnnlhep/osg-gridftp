bc2389774707c2a2b9ed47964e9658eafc4b0d72
Kevin Fox <Kevin.Fox@pnnl.gov>
2018-02-14 09:28:04 -0800

    Update the container to switch to osg 3.3, support skipping the old style init, and add wait and start/stop hooks.

--- start.sh
+++ start.sh
@@ -1,6 +1,27 @@
 #!/bin/bash -e
 
+if [ "x$SKIP_ENV" = "x" ]; then
 . /etc/setup.sh
+fi #end "x$SKIP_ENV" = "x" ]
 
-/usr/sbin/globus-gridftp-server -c /etc/gridftp.conf -C /etc/gridftp.d -pidfile /var/run/globus-gridftp-server.pid
+(
+if [ "x$START_WAIT_FILE" != "x" ]; then
+	while true; do
+		[ -f "$START_WAIT_FILE" ] && break
+		sleep 1
+	done
+fi
+if [ -f /etc/start.extra.sh ]; then
+  . /etc/start.extra.sh
+fi
+cd /tmp
+exec /usr/sbin/globus-gridftp-server -c /etc/gridftp.conf -C /etc/gridftp.d -pidfile /var/run/globus-gridftp-server.pid
+) & pid=$!
 
+trap "kill $pid" TERM
+echo $pid > /var/run/gridftp.pid
+if [ "x$START_WAIT_DONE_FILE" != "x" ]; then
+	touch "$START_WAIT_DONE_FILE"
+fi
+wait $pid
+[ -f /etc/shutdown.sh ] && bash /etc/shutdown.sh
